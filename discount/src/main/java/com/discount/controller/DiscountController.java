c79e719a45878823b710e07e76fe5fe6a811b206
fwz <1090488745@qq.com>
2018-12-20 09:15:22 +0800

    fwz

--- discount/src/main/java/com/discount/controller/DiscountController.java
+++ discount/src/main/java/com/discount/controller/DiscountController.java
@@ -2,8 +2,12 @@ package com.discount.controller;
 
 import com.discount.pojo.TDiscount;
 import com.discount.service.DiscountService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +16,10 @@ public class DiscountController {
     @Autowired
     private DiscountService discountService;
 
-    @RequestMapping(value = "update")
+    @ResponseBody
+    @RequestMapping(value = "upadte",method = RequestMethod.POST)
+    @ApiOperation(value = "优惠券",notes = "修改优惠券状态",response = String.class)
+    @ApiImplicitParam(paramType = "query",required = false,name = "discount",value = "优惠券")
     public String update(TDiscount tDiscount){
         String s = discountService.updateDiscount(tDiscount);
         return s;
