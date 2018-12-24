8bd2bdd750edf22d8b1b50854a99dbcbb8947a6d
fwz <1090488745@qq.com>
2018-12-24 17:31:10 +0800

    fwz

---
+++ problem/src/main/java/com/problem/pojo/TProblem.java
@@ -0,0 +1,35 @@
+package com.problem.pojo;
+
+
+public class TProblem {
+
+  private Integer question_Id;
+  private String question_Title;
+  private String question_Content;
+
+//常见问题
+
+  public Integer getQuestion_Id() {
+    return question_Id;
+  }
+
+  public void setQuestion_Id(Integer question_Id) {
+    this.question_Id = question_Id;
+  }
+
+  public String getQuestion_Title() {
+    return question_Title;
+  }
+
+  public void setQuestion_Title(String question_Title) {
+    this.question_Title = question_Title;
+  }
+
+  public String getQuestion_Content() {
+    return question_Content;
+  }
+
+  public void setQuestion_Content(String question_Content) {
+    this.question_Content = question_Content;
+  }
+}
