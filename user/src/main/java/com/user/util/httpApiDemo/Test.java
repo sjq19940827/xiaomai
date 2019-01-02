b14cda97fc012fdab9465e3fccef0e108066d2e6
zhengyihao <zhengyh998994@163.com>
2019-01-02 15:30:31 +0800

    zyh-01/02/15/30

--- user/src/main/java/com/user/util/httpApiDemo/Test.java
+++ user/src/main/java/com/user/util/httpApiDemo/Test.java
@@ -8,10 +8,14 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Jedis jedis = new Jedis("localhost",6379);
-		jedis.set("fsb","范文泽是傻逼");
-		jedis.get("fsb");
-		System.out.println(jedis.get("fsb") != null ? "y" : "e");
+		Jedis jedis = new Jedis();
+
+		String asd = "aaas";
+		jedis.set(asd,"范文泽是傻逼");
+		jedis.get(asd);
+		String asdf = "utf" + jedis.get(asd) + "asd";
+		jedis.set(asdf,"17254241325");
+		System.out.println(jedis.get(asdf));
 
 	}
 
